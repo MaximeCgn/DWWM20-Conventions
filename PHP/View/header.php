b79e44fc0d93f710d533e3d2e4d9aedfb8cedb31
MaximeCgn <maxime.cugny@laposte.net>
2020-11-26 11:25:26 +0100

    26 11 2020

--- PHP/View/header.php
+++ PHP/View/header.php
@@ -1,10 +1,9 @@
 <body class="colonne">
     <header>
         <div class="image">
-            <img src="../../IMG/image1.jpg" alt="image PGW">
+            <img src="./IMG/logo.png" alt="image PGW">
         </div>
         <div class="titre">Bienvenue sur le site des Conventions</div>
         <div class="image">
-            <img src="../../IMG/image2.jpg" alt="image PGW">
         </div>
     </header>
