780a4b09172a74c4cb976510d2b17ec40740bd34
S-Kirua26 <steevengheeraert26pgmail.com>
2020-11-26 14:35:16 +0100

    26/11/2020 SG

--- PHP/View/FormulaireVisite.php
+++ PHP/View/FormulaireVisite.php
@@ -1,9 +1,16 @@
 <?php
 
-echo '<body class="colonne">
+echo '<body class="colonne">';
 
-<form method="POST" action="index.php>?page=traitementVisite&typeTraitement=ajouter">
-<div class="contenu colonne">
+$typeFormulaire=$_GET["typeFormulaire"];
+if ($typeFormulaire=="ajouter")
+{
+    echo '<form method="POST" action="index.php>?page=traitementVisite&typeTraitement=ajouter">
+    $visite = new Visite(["dateVisite" => $_POST["dateVisite"], "idVisiteur" => $_POST["idVisiteur"], "idConvention" => $_POST["idConvention"])';
+    VisiteManager::add($visite);
+}
+
+echo '<div class="contenu colonne">
     <div class="colonne">
         <div class="titreColonne centre">
             Date de la visite
@@ -19,12 +26,16 @@ echo '<body class="colonne">
     <div class="colonne">
         <div class="titreColonne centre">
             Id Convention
-        </div>
-        <select class="libelle" name="idConvention">
-            <option value="1">test salle 1</option>
-            <option value="3"> test salle 3</option>
-        </select>
-    </div>
+        </div>';
+
+        foreach ($liste as $uneVisite)
+        {
+            echo '<select class="libelle" name="idConvention">
+                <option value="1">test salle 1</option>
+                <option value="3"> test salle 3</option>
+            </select>';
+        }
+    echo '</div>
     <div class="return marginLight"><a class="centre" href="index.php">Retour</a></div>
     <input type="submit" class="ajouter marginLight centre" name="submit" value="ajouter"/>
 </div>
--- PHP/View/ListeVisite.php
+++ PHP/View/ListeVisite.php
@@ -12,7 +12,7 @@ echo '<body class="colonne">
         </div>';
         foreach ($liste as $uneVisite)
         {
-            echo '<div class="liste">
+            echo '<div class="liste marginLight">
             <div class="libelle marginBouton">'.$uneVisite->getDateVisite().'</div>
             <div class="details marginBouton"><a class="centre" href="index.php?page=Detail&id='.$uneVisite->getIdVisite().'">Aller voir une visite</a></div>
             <div class="modifier marginBouton"><a class="centre" href="index.php?page=modifier&id='.$uneVisite->getIdVisite().'">Aller modifier une visite</a></div>
