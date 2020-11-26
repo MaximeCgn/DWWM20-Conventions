c0c5a7b6de4f4272beda895d77860089418ad1f0
cedric444 <cedricpietka@gmail.com>
2020-11-26 17:39:53 +0100

    26/11/2020 cp

--- PHP/View/ListeAnimateurs.php
+++ PHP/View/ListeAnimateurs.php
@@ -14,16 +14,16 @@ echo'<div class="contenu colonne">
 echo'<div class="liste">';
 foreach($listeAnim as $unAnim)
 {
-    echo'<div class="libelle marginBouton">'.$unAnim->getNomAnimateur().' '.$unAnim->getPrenom().'</div>
-            <div class="details marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=details&id='.$unAnim->getIdAnimateur().'>Details</a></div>
-            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=modifier&id='.$unAnim->getIdAnimateur().'>Modifier</a></div>
-            <div class="supprimer marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=supprimer&id='.$unAnim->getIdAnimateur().'>Supprimer</div>
-}';
+    echo'<div class="libelle marginBouton">'.$unAnim->getNomAnimateur().' '.$unAnim->getPrenomAnimateur().'</div>
+            <div class="details marginBouton"><a href="index.php?page=formulaireAnimateurs&typeTraitement=details&id='.$unAnim->getIdAnimateur().'>Details</a></div>
+            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeTraitement=modifier&id='.$unAnim->getIdAnimateur().'>Modifier</a></div>
+            <div class="supprimer marginBouton"><a href="index.php?page=formulaireAnimateurs&typeTraitement=supprimer&id='.$unAnim->getIdAnimateur().'>Supprimer</div>';
+}
            
 echo'</div>
         <div class="margin">
             <div class="espace"></div>
-            <div class="return"><a class="centre" href="index.php?page=formulaire">Retour</a></div>
+            <div class="return"><a class="centre" href="index.php?page=formulaireAnimateur">Retour</a></div>
             <div class="espace"></div>
         </div>
     </div>';
