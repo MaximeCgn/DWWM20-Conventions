fb716bf65592424e25af87330f2282bf9c28c0e5
SofianeAa <71269443+SofianeAa@users.noreply.github.com>
2020-11-26 15:00:46 +0100

    y fo komit

--- PHP/View/ListeEmplacements.php
+++ PHP/View/ListeEmplacements.php
@@ -7,17 +7,19 @@ echo '<body class="colonne">
     <div class="contenui colonne">
         <div class="margin">
             <div class="espace"></div>
-            <div class="ajouter"><a class="centre" href="index.php?page=formulaire">Ajouter convention</a></div>
+            <div class="ajouter"><a class="centre" href="index.php?page=formulaireEmplacements">Ajouter convention</a></div>
             <div class="espace"></div>
         </div>
         <div class="liste">';
             foreach ($liste as $unEmplacement)
             {
-            echo '<div class="libelle marginBouton">Libelle Conventions</div>
-            <div class="details marginBouton">Details</div>
-            div class="modifier marginBouton">Modifier</div>
-            <div class="supprimer marginBouton">Supprimer</div>'
-        }
+            echo '<div class="emplacement margin">
+            <div class="libelle marginBouton">'.$unEmplacement->getNomEmplacement().'</div>
+            <div class=details marginBouton"><a href="index.php?page=formulaireEmplacements&typeFormulaire=details&id='.$unEmplacement->getIdEmplacement().'">Détails</a></div>
+            <div class="modifier marginBouton"><a href="index.php?page=formulaireEmplacements&typeFormulaire=modifier&id='.$unEmplacement->getIdEmplacement().'">Modifier</a></div>
+            <div class="supprimer marginBouton"><a href="index.php?page=traitementEmplacements&typeTraitement=supprimer&id='.$unEmplacement->getIdEmplacement().'">Supprimer</a></div>
+            </div>';
+            }
 
         echo '</div>
 
