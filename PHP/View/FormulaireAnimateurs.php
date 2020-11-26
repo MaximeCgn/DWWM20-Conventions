5e8adc5362b2d0ee25b478dfaf09916392d81f5f
cedric444 <cedricpietka@gmail.com>
2020-11-26 22:44:03 +0100

    26/11/2020 cp

--- PHP/View/FormulaireAnimateurs.php
+++ PHP/View/FormulaireAnimateurs.php
@@ -99,46 +99,48 @@ else if($typeFormulaire=="modifier")
         else if($typeFormulaire=="details")
             {
                 $idRecherche= $_GET['id'];
-                $id= EmplacementsManager::findById($idRecherche);
-                $listeEmplacement= EmplacementsManager::getList();
+                $id= AnimateursManager::findById($idRecherche);
+                
                 echo'<form method="POST" action="index.php>?page=traitementAnimateurs&typeTraitement=details">
                 <div class="contenu colonne">
                     <div class="colonne">
-                        <div class="titreColonne centre">
+                        <div class="titreColonne colonne centre marginLight">
                             Nom de l\'animateur
                         </div>
-                        <input class="libelle" type="text" name="nomAnimateur" placeholder="Nom de l\'animateur"/>
+                        <input type="hidden" name="id" value="'.$id->getIdAnimateur().'" disabled/>
+                        <input type="text" name="nomAnimateur" value="'.$id->getNomAnimateur().'" disabled/>
                     </div>
                     <div class="colonne">
-                        <div class="titreColonne centre">
+                        <div class="titreColonne colonne centre marginLight">
                             Prénom de l\'animateur
                         </div>
-                        <input class="libelle" type="text" name="PrenomAnimateur" placeholder="Prenom de l\'animateur"/>
+                        <input class="libelle" type="text" name="PrenomAnimateur" value="'.$id->getPrenomAnimateur().'" disabled/>
                     </div>
                     <div class="colonne">
-                        <div class="titreColonne centre">
+                        <div class="titreColonne colonne centre marginLight">
                             Titre de l\'animateur
                             </div>
-                        <input class="libelle" type="text" name="TitreAnimateur" placeholder="Titre de l\'animateur"/>
+                        <input class="libelle" type="text" name="TitreAnimateur" value="'.$id->getTitreAnimateur().'" disabled/>
                     </div>
                     <div class="colonne">
-                        <div class="titreColonne centre">
+                        <div class="titreColonne colonne centre marginLight">
                             Date de début
                             </div>
-                        <input class="libelle" type="date" name="DateDebut" placeholder="Date de début"/>
+                        <input class="libelle" type="date" name="DateDebut" value="'.$id->getDateDebut().'" disabled/>
                     </div>
                     <div class="colonne">
-                        <div class="titreColonne centre">
+                        <div class="titreColonne colonne centre marginLight">
                             Date de fin
                             </div>
-                        <input class="libelle" type="date" name="DateFin" placeholder="Date de fin"/>
+                        <input class="libelle" type="date" name="DateFin" value="'.$id->getDateFin().'" disabled/>
                     </div>';
                     echo'</div>
                     <select class="libelle" name="idEmplacement">';
+                    $listeEmplacement=EmplacementsManager::getList();
                     foreach($listeEmplacement as $unEmplacement)
                     {
                         $sel="";
-                        if($unEmplacement->getIdEmplacement()== $id)
+                        if($unEmplacement->getIdEmplacement()== $idEmp)
                         {
                             $sel= "selected";
                         }
--- PHP/View/ListeAnimateurs.php
+++ PHP/View/ListeAnimateurs.php
@@ -1,9 +1,6 @@
 <?php
-
 $listeAnim= AnimateursManager::getList();
-
-
-
+echo'<div></div>';
 echo'<div class="contenu colonne">
         <div class="margin">
             <div class="espace"></div>
@@ -11,19 +8,13 @@ echo'<div class="contenu colonne">
             <div class="espace"></div>
         </div>';
 
-echo'<div class="liste">';
+echo'<div class="liste colonne">';
 foreach($listeAnim as $unAnim)
 {
-    echo'<div class="libelle marginBouton">'.$unAnim->getNomAnimateur().' '.$unAnim->getPrenomAnimateur().'</div>
-            <div class="details marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=details&id='.$unAnim->getIdAnimateur().'>Details</a></div>
-            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=modifier&id='.$unAnim->getIdAnimateur().'>Modifier</a></div>
-            <div class="supprimer marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=supprimer&id='.$unAnim->getIdAnimateur().'>Supprimer</div>';
+    echo'<div class="visiteur margin">
+        <div class="libelle marginBouton">'.$unAnim->getNomAnimateur().' '.$unAnim->getPrenomAnimateur().'</a></div>
+            <div class="details marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=details&id='.$unAnim->getIdAnimateur().'">Details</a></div>
+            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=modifier&id='.$unAnim->getIdAnimateur().'">Modifier</a></div>
+            <div class="supprimer marginBouton"><a href="index.php?page=formulaireAnimateurs&typeFormulaire=supprimer&id='.$unAnim->getIdAnimateur().'">Supprimer</a></div>
+        </div>';
 }
-           
-echo'</div>
-        <div class="margin">
-            <div class="espace"></div>
-            <div class="return"><a class="centre" href="index.php?page=formulaireAnimateur">Retour</a></div>
-            <div class="espace"></div>
-        </div>
-    </div>';
