58da6f42747e13615b7f6b5f3ecba636cd23ffd2
SofianeAa <71269443+SofianeAa@users.noreply.github.com>
2020-11-26 15:30:00 +0100

    correction

--- PHP/View/ListeEmplacements.php
+++ PHP/View/ListeEmplacements.php
@@ -14,7 +14,7 @@ echo '<body class="colonne">
             foreach ($liste as $unEmplacement)
             {
             echo '<div class="emplacement margin">
-            <div class="libelle marginBouton">'.$unEmplacement->getNomEmplacement().'</div>
+            <div class="libelle marginBouton">'.$unEmplacement->getnumeroEmplacement().'</div>
             <div class=details marginBouton"><a href="index.php?page=formulaireEmplacements&typeFormulaire=details&id='.$unEmplacement->getIdEmplacement().'">Détails</a></div>
             <div class="modifier marginBouton"><a href="index.php?page=formulaireEmplacements&typeFormulaire=modifier&id='.$unEmplacement->getIdEmplacement().'">Modifier</a></div>
             <div class="supprimer marginBouton"><a href="index.php?page=traitementEmplacements&typeTraitement=supprimer&id='.$unEmplacement->getIdEmplacement().'">Supprimer</a></div>
