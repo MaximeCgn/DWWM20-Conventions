93898b2ddaeb4609ddb7f4cb6635de0b5e92b060
cedric444 <cedricpietka@gmail.com>
2020-11-26 13:37:35 +0100

    '26/11/2020'

--- PHP/View/ListeAnimateurs.php
+++ PHP/View/ListeAnimateurs.php
@@ -16,14 +16,14 @@ foreach($listeAnim as $unAnim)
 {
     echo'<div class="libelle marginBouton">'.$unAnim->getNomAnimateur().' '.$unAnim->getPrenom().'</div>
             <div class="details marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=details&id='.$unAnim->getIdAnimateur().'>Details</a></div>
-            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=Modifier&id='.$unAnim->getIdAnimateur().'>Modifier</a></div>
-            <div class="supprimer marginBouton">Supprimer</div>';
-}
+            <div class="modifier marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=modifier&id='.$unAnim->getIdAnimateur().'>Modifier</a></div>
+            <div class="supprimer marginBouton"><a href="index.php?page=formulaireAnimateurs&typeForm=supprimer&id='.$unAnim->getIdAnimateur().'>Supprimer</div>
+}';
            
-        </div>
+echo'</div>
         <div class="margin">
             <div class="espace"></div>
             <div class="return"><a class="centre" href="index.php?page=formulaire">Retour</a></div>
             <div class="espace"></div>
         </div>
-    </div>
+    </div>';
