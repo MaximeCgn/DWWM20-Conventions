55fc97b4bfc12930e3b59c28303031e10e90f9b2
MaximeCgn <maxime.cugny@laposte.net>
2020-11-26 15:52:51 +0100

    26 11 2020 Maxime Cgn

--- PHP/View/ListeVisiteurs.php
+++ PHP/View/ListeVisiteurs.php
@@ -4,7 +4,7 @@ echo '<div></div>';
 echo '<div class="contenu colonne">
 <div class="margin">
     <div class="espace"></div>
-    <div class="ajouter"><a class="centre" href="index.php?page=formulaire">Ajouter Convention</a></div>
+    <div class="ajouter"><a class="centre" href="index.php?page=formulaireVisiteurs&typeFormulaire=ajouter">Ajouter Visiteur</a></div>
     <div class="espace"></div>
 </div>';
 
@@ -13,9 +13,9 @@ foreach ($visiteurs as $unVisiteur){
     echo '
     <div class="visiteur margin">
     <div class="libelle marginBouton">'. $unVisiteur->getNomVisiteur().' '.$unVisiteur->getPrenomVisiteur().'</div>
-    <div class="details marginBouton"><a href="index.php?page=formulaire&typeFormulaire=details&id='.$unVisiteur->getIdVisiteur().'">Détails</a></div>
-    <div class="modifier marginBouton"><a href="index.php?page=formulaire&typeFormulaire=modifier&id='.$unVisiteur->getIdVisiteur().'">Modifier</a></div>
-    <div class="supprimer marginBouton"><a href="index.php?page=traitement&typeTraitement=supprimer&id='.$unVisiteur->getIdVisiteur().'">Supprimer</a></div>
+    <div class="details marginBouton"><a href="index.php?page=formulaireVisiteurs&typeFormulaire=details&id='.$unVisiteur->getIdVisiteur().'">Détails</a></div>
+    <div class="modifier marginBouton"><a href="index.php?page=formulaireVisiteurs&typeFormulaire=modifier&id='.$unVisiteur->getIdVisiteur().'">Modifier</a></div>
+    <div class="supprimer marginBouton"><a href="index.php?page=traitementVisiteurs&typeTraitement=supprimer&id='.$unVisiteur->getIdVisiteur().'">Supprimer</a></div>
     </div>
     ';
 }
